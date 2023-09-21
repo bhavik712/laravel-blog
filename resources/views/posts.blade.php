0acd6912525a153dd338bc80b2387f4d55947591
bhavikb <bhavikb@weblight.co.in>
2023-09-21 19:44:37 +0530

    Get HTML - css template used it dynamically for posts page & single page post Learned How to give props and dynamic style to common component Get idea of creating common component for Author name, category etc.

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,14 +1,27 @@
 <x-layout>
-    @foreach ($posts as $post) 
-        <article class="article">
-            <div class="title"><a href="/posts/{{$post->id}}">{{$post->title}}</a></div>
-            <div class="category">By <a href="/authors/{{ $post->author->name }}">{{ $post->author->name  }}</a> in <a href="/categories/{{ $post->category->name }}">{{ $post->category->name  }}</a></div>
-            <div class="body">
-                {{ $post->excerpt }}
+
+    <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+        {{-- article 1 --}}
+        @if ($posts->count())
+            <x-post-feature :post="$posts[0]">
+            </x-post-feature>
+        @else
+            <p>No posts yet.</p>
+            
+        @endif
+        
+        @if ($posts->count() > 1)
+            <div class="lg:grid lg:grid-cols-6">
+                @foreach ($posts->skip(1) as $post)
+                    <x-post-card :post="$post" class="{{ $loop->iteration < 3 ? 'col-span-3' : 'col-span-2' }}">
+                        
+                    </x-post-card>
+                    
+                @endforeach
             </div>
-        </article>
-                
-    @endforeach
+        @endif
+        
+    </main>
 
 
 </x-layout>
